d79f0db206ca25c8705650a4b74653eec46c2bb0
BeomSeogKim <kbs4520@daum.net>
2022-09-26 10:29:20 +0900

    Refactoring

--- src/main/java/Backend/FinalProject/dto/PostResponseDto.java
+++ src/main/java/Backend/FinalProject/dto/PostResponseDto.java
@@ -1,16 +1,12 @@
 package Backend.FinalProject.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
-
-import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 
 @Builder
 @Getter
@@ -21,14 +17,14 @@ public class PostResponseDto {
     private Long id;
     private String title;
     private String author;
+    private String memberImgUrl;
     private String address;
     private String content;
     private int maxNum;
     private String restDay;
     private LocalDate dDay;
-    private String postimgUrl;
-    private String memberimgUrl;
-    private List<CommentResponseDto> commentResponseDtoList;
+    private String postImgUrl;
+    private List<CommentResponseDto> commentList;
     private LocalDate startDate;
     private LocalDate endDate;
 
