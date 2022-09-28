21942799adf790d4dd0f384fdec702d31b5ce03a
BeomSeogKim <kbs4520@daum.net>
2022-09-29 00:00:35 +0900

    지원 신청 버그 수정

--- src/main/java/Backend/FinalProject/service/ApplicationService.java
+++ src/main/java/Backend/FinalProject/service/ApplicationService.java
@@ -115,10 +115,10 @@ public class ApplicationService {
         if (application.getStatus() == ApplicationState.APPROVED) {
             return ResponseDto.fail("ALREADY APPROVED", "이미 수락을 하셨습니다.");
         }
-        if (application.getPost().getCurrentNum() <= application.getPost().getMaxNum()) {
+        if (application.getPost().getCurrentNum() < application.getPost().getMaxNum()) {
             application.approve();
         }
-        if (application.getPost().getCurrentNum() > application.getPost().getMaxNum()) {
+        if (application.getPost().getCurrentNum() >= application.getPost().getMaxNum()) {
             return ResponseDto.fail("OVER MAX_NUM", "정원을 초과하였습니다.");
         }
         return ResponseDto.success("성공적으로 승인이 되었습니다.");
