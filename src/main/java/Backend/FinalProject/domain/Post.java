ac5170897a5ee6d866d76bf6eb3238f5f3a348a5
BeomSeogKim <kbs4520@daum.net>
2022-09-29 00:28:34 +0900

    데이터 연관관계 수정

--- src/main/java/Backend/FinalProject/domain/Post.java
+++ src/main/java/Backend/FinalProject/domain/Post.java
@@ -57,7 +57,7 @@ public class Post extends Timestamped{
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> commentList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Application> applicationList = new ArrayList<>();
 
     @OneToMany(mappedBy = "post")
