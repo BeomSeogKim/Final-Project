4892f6a0727b57ca965a7998db5a1fdefb1d3a7d
BeomSeogKim <kbs4520@daum.net>
2022-10-18 18:08:16 +0900

    Exception Handler 설정 #160

--- src/main/java/Backend/FinalProject/exception/ErrorCode.java
+++ src/main/java/Backend/FinalProject/exception/ErrorCode.java
@@ -1,15 +1,15 @@
-//package Backend.FinalProject.exception;
-//
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//
-//@Getter
-//@RequiredArgsConstructor
-//public enum ErrorCode {
-//    private final HttpStatus status;
-//    private final String errorCode;
-//    private final String errorMessage;
-//
-//    NOT_VALIDCONTENT(HttpStatus.BAD_REQUEST,"400","유효하지 않는 내용입니다.")
-//}
+package Backend.FinalProject.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    NOT_VALIDCONTENT(HttpStatus.NOT_FOUND,"400","유효하지 않는 내용입니다.");
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String errorMessage;
+}
