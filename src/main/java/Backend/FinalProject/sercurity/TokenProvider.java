f888308d863082bdd0aa5c918db5c9f5bbae4943
BeomSeogKim <kbs4520@daum.net>
2022-10-20 01:43:11 +0900

    FE 테스트

--- src/main/java/Backend/FinalProject/sercurity/TokenProvider.java
+++ src/main/java/Backend/FinalProject/sercurity/TokenProvider.java
@@ -31,11 +31,11 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Component
-public class TokenProvider {
+public class rTokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            //30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30 / 30;            //30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;     //7일
 
     private final Key key;
