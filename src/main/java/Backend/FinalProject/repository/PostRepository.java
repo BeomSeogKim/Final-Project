6b1b3f2765049e9ca019ae7f86894256c5a16ecc
BeomSeogKim <kbs4520@daum.net>
2022-10-19 17:57:30 +0900

    Query 추가 #113

--- src/main/java/Backend/FinalProject/repository/PostRepository.java
+++ src/main/java/Backend/FinalProject/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package Backend.FinalProject.repository;
 
 import Backend.FinalProject.domain.Post;
+import Backend.FinalProject.domain.enums.PostState;
+import Backend.FinalProject.domain.enums.Regulation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +16,10 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-
     Optional<Post> findById(Long id);
     List<Post> findAllByMemberId(Long memberId);
     Page<Post> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    @Query(value = "select count(p) from Post p where p.regulation = :regulation or p.status =:status or p.status = :status2")
+    long findAllHiddenPost(@Param("regulation") Regulation regulation, @Param("status") PostState postState, @Param("status2") PostState postState2);
 }
