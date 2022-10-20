7e68775d5453a79a668f4b336bb5f506206430f9
BeomSeogKim <kbs4520@daum.net>
2022-10-20 21:39:06 +0900

    채팅 읽음 count 관련 domain 추가 생성 #45

---
+++ src/main/java/Backend/FinalProject/WebSocket/domain/ReadCheck.java
@@ -0,0 +1,45 @@
+package Backend.FinalProject.WebSocket.domain;
+
+import Backend.FinalProject.domain.Timestamped;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Slf4j
+public class ReadCheck extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "chat_member", nullable = false)
+    private ChatMember chatMember;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "chat_message_id")
+    private ChatMessage chatMessage;
+
+
+    //== 연관관계 메서드 ==//
+    public void setChatMessage(ChatMessage chatMessage) {
+        this.chatMessage = chatMessage;
+    }
+
+    public void setChatMember(ChatMember chatMember) {
+        this.chatMember = chatMember;
+        chatMember.getReadCheckList().add(this);
+    }
+}
