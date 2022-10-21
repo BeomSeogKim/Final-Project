d355199048c3501180f00d332d65083e6f6a2ab9
BeomSeogKim <kbs4520@daum.net>
2022-10-21 15:29:04 +0900

    TEST 주소 추가

--- src/main/java/Backend/FinalProject/WebSocket/controller/ChatController.java
+++ src/main/java/Backend/FinalProject/WebSocket/controller/ChatController.java
@@ -4,13 +4,16 @@ import Backend.FinalProject.WebSocket.domain.dtos.ChatInformationDto;
 import Backend.FinalProject.WebSocket.service.ChatService;
 import Backend.FinalProject.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RequiredArgsConstructor
 @Controller
+@Slf4j
 public class ChatController {
 
     //== Dependency Injection ==//
@@ -18,11 +21,21 @@ public class ChatController {
 
     /**
      * WebSocket 실시간 통신 채널
-     * @param message : 방정보 및 전달 메세지 정보
+     *
+     * @param message     : 방정보 및 전달 메세지 정보
      * @param accessToken : 회원 검증을 위한 AccessToken  받기
      */
     @MessageMapping("/chat/message")
     public ResponseDto<?> sendMessage(ChatInformationDto message, @Header("Authorization") String accessToken) {
         return chatService.sendMessage(message, accessToken);
+    }
+
+    @PostMapping("/test")
+    public ResponseDto<?> checktest() {
+
+        log.info("test 성공");
+
+
+        return ResponseDto.success("테스트 성공");
     }
 }
