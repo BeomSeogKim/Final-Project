ae5edde9f96ecaa7c2ac45a8ce8133cf9520c4cf
BeomSeogKim <kbs4520@daum.net>
2022-10-22 18:52:17 +0900

    로그 추적

--- src/main/java/Backend/FinalProject/sse/service/NotificationService.java
+++ src/main/java/Backend/FinalProject/sse/service/NotificationService.java
@@ -131,10 +131,13 @@ public class NotificationService {
     // 유효시간이 다 지난다면 503 에러가 발생하기 때문에 더미데이터를 발행
 
     private void sendNotification(SseEmitter emitter, String eventId, String emitterId, Object data) {
+        log.info("전송 시작");
+        log.info(emitter.toString());
         try {
             emitter.send(SseEmitter.event()
                     .id(eventId)
                     .data(data));
+            log.info(String.valueOf(SseEmitter.event().id(eventId).data(data)));
         } catch (IOException exception) {
             emitterRepository.deleteById(emitterId);
         }
