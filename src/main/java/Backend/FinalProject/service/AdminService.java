583f6a5c0a53671648bc562e9d8cbe28780252b3
BeomSeogKim <kbs4520@daum.net>
2022-11-09 00:09:09 +0900

    Admin Service Refactoring #113

--- src/main/java/Backend/FinalProject/service/AdminService.java
+++ src/main/java/Backend/FinalProject/service/AdminService.java
@@ -1,12 +1,15 @@
 package Backend.FinalProject.service;
 
 import Backend.FinalProject.Tool.Validation;
-import Backend.FinalProject.domain.*;
-import Backend.FinalProject.dto.response.report.ReportListDto;
+import Backend.FinalProject.domain.Comment;
+import Backend.FinalProject.domain.Member;
+import Backend.FinalProject.domain.Post;
+import Backend.FinalProject.domain.Report;
+import Backend.FinalProject.dto.ResponseDto;
 import Backend.FinalProject.dto.response.report.ReportCommentDto;
+import Backend.FinalProject.dto.response.report.ReportListDto;
 import Backend.FinalProject.dto.response.report.ReportMemberDto;
 import Backend.FinalProject.dto.response.report.ReportPostDto;
-import Backend.FinalProject.dto.ResponseDto;
 import Backend.FinalProject.repository.CommentRepository;
 import Backend.FinalProject.repository.MemberRepository;
 import Backend.FinalProject.repository.PostRepository;
@@ -20,6 +23,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
+import static Backend.FinalProject.Tool.Validation.handleBoolean;
+import static Backend.FinalProject.Tool.Validation.handleNull;
+import static Backend.FinalProject.domain.enums.ErrorCode.ADMIN_BAD_REQUEST;
 import static Backend.FinalProject.domain.enums.ReportStatus.UNDONE;
 import static Backend.FinalProject.domain.enums.ShowStatus.SHOW;
 
@@ -28,110 +34,67 @@ import static Backend.FinalProject.domain.enums.ShowStatus.SHOW;
 @Slf4j
 public class AdminService {
 
+    //== Dependency Injection ==//
     private final ReportRepository reportRepository;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
-
-
-
     private final Validation validation;
 
-    public ResponseDto<?> getReportList(HttpServletRequest request) {
-        validation.checkAccessToken(request);
+    /**
+     * 신고 내역 조회
+     * @param httpServletRequest : HttpServlet Request
+     */
+    @Transactional(readOnly = true)
+    public ResponseDto<?> getReportList(HttpServletRequest httpServletRequest) {
+        validation.checkAccessToken(httpServletRequest);
 
-        List<Report> reportMemberList = reportRepository.findByMember();
+        List<Report> reportMemberList = reportRepository.findByMember(UNDONE, SHOW);
         List<ReportMemberDto> reportMember = new ArrayList<>();
-
-        for(Report report : reportMemberList) {
-            if (report.getReportStatus().equals(UNDONE) && report.getShowStatus().equals(SHOW)) {
-              Member optionalMember = memberRepository.findById(report.getMemberId()).orElse(null);
-              if(optionalMember==null){
-                  log.info("AdminService getReportList NOT FOUND");
-                  return ResponseDto.fail("BAD REQUEST", "올바르지 않은 접근입니다.");
-              }
-              reportMember.add(
-                      ReportMemberDto.builder()
-                              .reportId(report.getId())
-                              .memberId(report.getMemberId())
-                              .content(report.getContent())
-                              .memberImgUrl(optionalMember.getImgUrl())
-                              .reportNickname(optionalMember.getNickname())
-                              .build());
-            }
+        for (Report report : reportMemberList) {
+            Member optionalMember = memberRepository.findById(report.getMemberId()).orElse(null);
+            handleNull(optionalMember, ADMIN_BAD_REQUEST);
+            addReportMember(reportMember, report, optionalMember);
         }
 
-        List<Report> reportPostList = reportRepository.findByPost();
-
+        List<Report> reportPostList = reportRepository.findByPost(UNDONE, SHOW);
         List<ReportPostDto> reportPost = new ArrayList<>();
-
         for (Report report : reportPostList) {
-            if (report.getReportStatus().equals(UNDONE) && report.getShowStatus().equals(SHOW)) {
-              Member optionalMember = memberRepository.findById(report.getReportMemberId()).orElse(null);
-              Post optionalPost = postRepository.findById(report.getPostId()).orElse(null);
-              if(optionalMember==null || optionalPost==null){
-                  log.info("AdminService getReportList NOT FOUND");
-                  return ResponseDto.fail("BAD REQUEST", "올바르지 않은 접근입니다.");
-              }
-              reportPost.add(
-                      ReportPostDto.builder()
-                              .reportId(report.getId())
-                              .postId(report.getPostId())
-                              .content(report.getContent())
-                              .postUrl(optionalPost.getImgUrl())
-                              .reportNickname(optionalMember.getNickname())
-                              .build());
-            }
+            Member optionalMember = memberRepository.findById(report.getReportMemberId()).orElse(null);
+            Post optionalPost = postRepository.findById(report.getPostId()).orElse(null);
+            handleBoolean(optionalMember == null || optionalPost == null, ADMIN_BAD_REQUEST);
+            addReportPost(reportPost, report, optionalMember, optionalPost);
         }
 
-        List<Report> reportCommentList = reportRepository.findByComment();
-
+        List<Report> reportCommentList = reportRepository.findByComment(UNDONE, SHOW);
         List<ReportCommentDto> reportComment = new ArrayList<>();
-
         for (Report report : reportCommentList) {
-          if (report.getReportStatus().equals(UNDONE) && report.getShowStatus().equals(SHOW)) {
-              Comment comment = commentRepository.findById(report.getCommentId()).orElse(null);
-              if (comment == null) {
-                  log.info("AdminService getReportList NOT FOUND");
-                  return ResponseDto.fail("BAD REQUEST", "올바르지 않은 접근입니다.");
-              }
-              Post post = postRepository.findById(report.getReportPostId()).orElse(null);
-              if (post ==null) {
-                  log.info("AdminService getReportList NOT FOUND");
-                  return ResponseDto.fail("BAD REQUEST", "올바르지 않은 접근입니다.");
-              }
-              Member member = memberRepository.findById(comment.getMember().getId()).orElse(null);
-              if (member == null) {
-                  log.info("AdminService getReportList NOT FOUND");
-                  return ResponseDto.fail("BAD REQUEST", "올바르지 않은 접근입니다.");
-              }
+            Comment comment = commentRepository.findById(report.getCommentId()).orElse(null);
+            handleNull(comment, ADMIN_BAD_REQUEST);
 
-              reportComment.add(
-                      ReportCommentDto.builder()
-                              .reportId(report.getId())
-                              .postId(report.getReportPostId())
-                              .commentId(report.getCommentId())
-                              .content(report.getContent())
-                              .memberUrl(member.getImgUrl())
-                              .nickname(member.getNickname())
-                              .postUrl(post.getImgUrl())
-                              .reportCommentContent(comment.getContent())
-                              .build());
-          }
+            Post post = postRepository.findById(report.getReportPostId()).orElse(null);
+            handleNull(post, ADMIN_BAD_REQUEST);
+
+            Member member = memberRepository.findById(comment.getMember().getId()).orElse(null);
+            handleNull(member, ADMIN_BAD_REQUEST);
+
+            addReportComment(reportComment, report, comment, post, member);
         }
-        
-        ReportListDto reportList = ReportListDto.builder()
-                .memberList(reportMember)
-                .postList(reportPost)
-                .commentList(reportComment)
-                .build();
+
+        ReportListDto reportList = makeReportList(reportMember, reportPost, reportComment);
 
         return ResponseDto.success(reportList);
 
     }
+
+    /**
+     * 제재 처리
+     * @param reportId : 신고 번호
+     * @param httpServletRequest : HttpServlet Request
+     */
     @Transactional
-    public ResponseDto<?> executeReport(Long reportId, HttpServletRequest request) {
-        validation.checkAccessToken(request);
+    public ResponseDto<?> executeReport(Long reportId, HttpServletRequest httpServletRequest) {
+        validation.checkAccessToken(httpServletRequest);
         Report report = reportRepository.findById(reportId).orElse(null);
         assert report != null;
         if (report.getMemberId() != null) {
@@ -145,19 +108,16 @@ public class AdminService {
             post.executeRegulation();
             postRepository.flush();
             // 같은 아이디를 가진 게시글 신고글들 다 HIDE 처리
-            List<Report> samePostReport = reportRepository.findAllByPostId(report.getPostId());
-            samePostReport.forEach(Report::hide);
+            reportRepository.findAllByPostId(report.getPostId()).forEach(Report::hide);
             // 같은 아이디를 가진 댓글 신고들 다 UNDONE 처리
-            List<Report> samePostCommentReport = reportRepository.findAllByReportPostId(report.getPostId());
-            samePostCommentReport.forEach(Report::hide);
+            reportRepository.findAllByReportPostId(report.getPostId()).forEach(Report::hide);
         } else {
             Comment comment = commentRepository.findById(report.getCommentId()).orElse(null);
             assert comment != null;
             comment.executeRegulation();
             commentRepository.flush();
             // 같은 댓글을 가진 댓글 신고들 다 Undone 처리
-            List<Report> sameCommentReport = reportRepository.findALlByCommentId(report.getCommentId());
-            sameCommentReport.forEach(Report::hide);
+            reportRepository.findALlByCommentId(report.getCommentId()).forEach(Report::hide);
 
         }
         report.updateStatus();
@@ -165,10 +125,60 @@ public class AdminService {
         return ResponseDto.success("성공적으로 처리 되었습니다.");
     }
 
+    /**
+     * 반려 처리
+     * @param reportId : 신고 번호
+     * @param httpServletRequest : HttpServlet Request
+     */
     @Transactional
-    public ResponseDto<?> withdrawReport(Long reportId, HttpServletRequest request) {
-        validation.checkAccessToken(request);
+    public ResponseDto<?> withdrawReport(Long reportId, HttpServletRequest httpServletRequest) {
+        validation.checkAccessToken(httpServletRequest);
         reportRepository.deleteById(reportId);
         return ResponseDto.success("성공적으로 처리 되었습니다.");
+    }
+
+    private static void addReportMember(List<ReportMemberDto> reportMember, Report report, Member optionalMember) {
+        reportMember.add(
+                ReportMemberDto.builder()
+                        .reportId(report.getId())
+                        .memberId(report.getMemberId())
+                        .content(report.getContent())
+                        .memberImgUrl(optionalMember.getImgUrl())
+                        .reportNickname(optionalMember.getNickname())
+                        .build());
+    }
+
+    private static void addReportPost(List<ReportPostDto> reportPost, Report report, Member optionalMember, Post optionalPost) {
+        reportPost.add(
+                ReportPostDto.builder()
+                        .reportId(report.getId())
+                        .postId(report.getPostId())
+                        .content(report.getContent())
+                        .postUrl(optionalPost.getImgUrl())
+                        .reportNickname(optionalMember.getNickname())
+                        .build());
+    }
+
+    private static void addReportComment(List<ReportCommentDto> reportComment, Report report, Comment comment, Post post, Member member) {
+        reportComment.add(
+                ReportCommentDto.builder()
+                        .reportId(report.getId())
+                        .postId(report.getReportPostId())
+                        .commentId(report.getCommentId())
+                        .content(report.getContent())
+                        .memberUrl(member.getImgUrl())
+                        .nickname(member.getNickname())
+                        .postUrl(post.getImgUrl())
+                        .reportCommentContent(comment.getContent())
+                        .build());
+    }
+
+    private static ReportListDto makeReportList(List<ReportMemberDto> reportMember, List<ReportPostDto> reportPost, List<ReportCommentDto> reportComment) {
+        ReportListDto reportList = ReportListDto.builder()
+                .memberList(reportMember)
+                .postList(reportPost)
+                .commentList(reportComment)
+                .build();
+        return reportList;
     }
 }
